15d13d24514ed407182514e6298c30f1751ead65
Mike Mattozzi <mike.mattozzi@gmail.com>
2025-02-08 00:08:43 -0500

    Adding face detection option to verify script -- to only search for files that contain a face

--- verify-photo-library.py
+++ verify-photo-library.py
@@ -5,6 +5,9 @@ import shutil
 import argparse
 import hashlib
 import re
+import cv2
+
+FILE_TYPES = [ "jpg", "JPG", "jpeg", "JPEG", "mp4", "MP4", "mov", "MOV" ]
 
 def calculate_md5(file_path):
     hash_md5 = hashlib.md5()
@@ -13,7 +16,32 @@ def calculate_md5(file_path):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-def verify_directory_contents(source_dir, dest_dir, ignore_pattern):
+def detect_faces(image_path):
+    global face_cascade
+
+    try:
+        face_cascade
+    except NameError:
+        # Load the Haar Cascade model
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Read the image
+    img = cv2.imread(image_path)
+    if img is None:
+        print("Error: Could not read image.")
+        return
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Return true if faces are detected
+    return len(faces) > 0
+
+
+def verify_directory_contents(source_dir, dest_dir, ignore_pattern, faces_only):
     print(f"Verifying files in {source_dir} are present in {dest_dir}")
 
     source_files = dict()
@@ -27,20 +55,29 @@ def verify_directory_contents(source_dir, dest_dir, ignore_pattern):
 
     print(f"Scanning source directory: {source_dir}")
     files_processed = 0
+    files_with_faces = 0
     for root, dirs, files in os.walk(source_dir):
         for file in files:            
             if file.startswith('.'):
                 continue
             source_file_path = os.path.join(root, file)
-            if ignore_pattern and ignore_regex.search(source_file_path):
-                continue
-            file_md5 = calculate_md5(source_file_path)
-            source_files[file_md5] = source_file_path
+            if any(file.endswith(file_type) for file_type in FILE_TYPES):
+                if ignore_pattern and ignore_regex.search(source_file_path):
+                    continue
+                if faces_only and detect_faces(source_file_path):
+                    file_md5 = calculate_md5(source_file_path)
+                    source_files[file_md5] = source_file_path
+                    files_with_faces += 1
+            
             files_processed += 1
-            print(f"\rFiles scanned: {files_processed}", end="")
+            if faces_only:
+                print(f"\rFiles scanned: {files_processed} - Found {files_with_faces} files with faces", end="")
+            else:
+                print(f"\rFiles scanned: {files_processed}", end="")
 
     print();
     print(f"Found {len(source_files)} unique files in source directory")
+    
 
     print(f"Scanning destination directory: {dest_dir}")
     files_processed = 0
@@ -77,6 +114,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--source_dir', required=True, help="Source directory containing photos")
     parser.add_argument('-d', '--dest_dir', required=True, help="Destination directory to copy organized photos")
     parser.add_argument("-i", "--ignore", help="Ignore source files matching the given pattern")
+    parser.add_argument("--faces-only", action="store_true", help="Only check for files that are likely to contain faces")
     
     args = parser.parse_args()
-    verify_directory_contents(args.source_dir, args.dest_dir, args.ignore)
+    verify_directory_contents(args.source_dir, args.dest_dir, args.ignore, args.faces_only)
